0d0ea767309e36aefd8ecb75a356e183404ee194
Krzysztof Skrzętnicki <gtener@gmail.com>
2012-12-17 17:38:33 +0100

    switch between hnn-0.1 and hnn-0.2 with cabal flag

--- lib/NeuralNets.hs
+++ lib/NeuralNets.hs
@@ -1,10 +1,15 @@
-{-# LANGUAGE ViewPatterns #-}
+{-# LANGUAGE CPP, ViewPatterns #-}
 
 module NeuralNets where
 
+#if NEWHNN
+import AI.HNN.FF.Network -- from hnn-0.2 package
+import qualified Data.Vector.Unboxed as U
+#else
 import AI.HNN.Neuron -- from hnn-0.1 package
 import AI.HNN.Net
 import AI.HNN.Layer
+#endif
 
 
 import Control.Monad(when)
@@ -15,19 +20,26 @@ import Data.List.Split(splitPlaces)
 import Board
 import CommonDatatypes
 
-data AgentNN = AgentNN { net :: [[Neuron]]
-                       , lastLayer :: [Neuron]
+#if !NEWHNN
+type Network a = [[Neuron]]
+#endif
+
+
+data AgentNN = AgentNN { 
+                         net :: Network Double
+                       , lastLayer :: [(Double,[Double])]
                        , col :: Color
                        }
+
+
 
 instance Agent AgentNN where
     mkAgent col = do
       neuralNetwork <- parseNetFromFile'
-      print ("neural network size", length neuralNetwork, (map length neuralNetwork))
       return (AgentNN neuralNetwork [] col)
     makeMove agent brd = do
       let gst = GameState brd (\ g -> doubleToEvalInt $ evalBoardNet (gtColorNow g) (gtBoard g) (net agent)) (col agent) (col agent)
-          depth = 3
+          depth = 1
           (princ, score) = GTreeAlgo.negascout gst depth
       -- when (True) (print ("agent-nn",score,col agent))
       return (gtBoard $ head $ tail $ princ)
@@ -37,8 +49,8 @@ doubleToEvalInt d = round (d * 10000)
 
 parseNetFromFile' = parseNetFromFile `fmap` readFile "/home/tener/nn.txt"
 
-parseNetFromFile :: String -> [[Neuron]]
-parseNetFromFile input = asserts $ neurons -- (length weights, length weights'split, length biases, neuronCount, layerCount, sizes)
+parseNetFromFile :: String -> Network Double
+parseNetFromFile input = asserts $ result -- (length weights, length weights'split, length biases, neuronCount, layerCount, sizes)
   where input'lines@(sizes'row : rest'rows) = lines input
         sizes = readIntsRow sizes'row -- sizes in first line
 
@@ -54,8 +66,18 @@ parseNetFromFile input = asserts $ neurons -- (length weights, length weights'sp
         weights'split = splitPlaces sizes weights
 
         --- weights, biases, neuronCount, layerCount, sizes
+        biases'neg = (map (map negate) biases)  -- must negate biases -- different from matlab
 
-        neurons = zipWith makeOneLayer (map (map negate) biases) weights'split -- must negate biases -- different from matlab
+#if NEWHNN
+        result = network
+        network = loadNetwork biases'neg weights'split
+#else
+        result = neurons
+        neurons = zipWith makeOneLayer biases'neg weights'split
+        -- helper
+        makeOneLayer :: [Double] -> [[Double]] -> [Neuron]
+        makeOneLayer biases weights = zipWith createNeuronSigmoid biases weights
+#endif
 
         asserts r | garbage /= [] = error (printf "parseNetFromFile: garbage not empty: %d elements" (length garbage))
                   | length weights /= neuronCount = error (printf "parseNetFromFile: too little weights: %d (should be %d)" (length weights) neuronCount)
@@ -63,18 +85,14 @@ parseNetFromFile input = asserts $ neurons -- (length weights, length weights'sp
                   | otherwise = r -- r for result
 
 
-        -- helper
-        makeOneLayer :: [Double] -> [[Double]] -> [Neuron]
-        makeOneLayer biases weights = zipWith createNeuronSigmoid biases weights
-
 readDoublesRow :: String -> [Double]
 readDoublesRow row = map read (words row)
 
 readIntsRow :: String -> [Int]
 readIntsRow row = map read (words row)
 
-evalBoardNet :: Color -> Board -> [[Neuron]] -> Double
-evalBoardNet col brd net = combine (computeNet'long net (map i2d $ boardToSparse brdEval))
+evalBoardNet :: Color -> Board -> Network Double -> Double
+evalBoardNet col brd net = result
     where
       brdEval = if col == White then brd else negateBoard brd
 
@@ -82,7 +100,16 @@ evalBoardNet col brd net = combine (computeNet'long net (map i2d $ boardToSparse
       i2d 0 = 0.0
       i2d _ = error "This function is not defined for values other than 1 and 0."
 
-      combine = sum -- TODO: better function!
+      values = map i2d $ boardToSparse brdEval
+ 
+
+#if NEWHNN
+      combine = U.sum -- TODO: better function!
+      result = combine (computeNetworkWith net sigmoid (U.fromList values))
+#else
+      combine = sum
+      result = combine (computeNet'long net values)
+#endif
 
 g0 :: (Num a) => [a]
 g0 = [1,0,1,0,0,0,0,0,1,1,1,1,0,1,1,0,1,1,1,0,1,1,1,0,1,0,0,1,0,1,1,1,1,1,0,1,0,0,1,0,0,1,0,1,1,1,0,0,1,1,0,1,0,0,1,0,1,1,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,1,0,0,0,1,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,1,0,0,1,0,1,0,0,0,0,0,0,0,1,0,1,1,0,0,0,0,1,1,0,0,1,0,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,1,0,1,0,0,1,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,1,0,0,0,0,0]
--- makefile
+++ makefile
@@ -4,8 +4,8 @@ build:
 
 add-sources:
 	cabal-dev add-source HNN-0.1/dist/hnn-0.1.2.3.4.tar.gz
+	cabal-dev add-source hnn/dist/hnn-0.2.0.0.20121217.tar.gz
 	cabal-dev add-source hashable-1.2.0.2/dist/hashable-1.2.0.2.20121217.tar.gz
-
 test:
 	cabal-dev install -j -fcairo --enable-tests
 prof:
