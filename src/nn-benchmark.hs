03a78cd937000dea2cc3427921bddec4df6a9290
Krzysztof Skrzętnicki <gtener@gmail.com>
2012-12-21 10:45:53 +0100

    nn-benchmark changes...

--- src/nn-benchmark.hs
+++ src/nn-benchmark.hs
@@ -8,20 +8,12 @@ import System.Random.MWC
 import Text.Printf
 import Data.List.Split(splitPlaces)
 
-#ifdef BENCH01
-#error BENCH01 not finished
-#endif
-
-#ifdef BENCH02
-import AI.HNN.FF.Network
+import AI.HNN.FF.Network as HNN
 import qualified Data.Vector.Unboxed as U
-#endif
 
-#ifdef BENCH03
-import AI.Network
-import AI.Calculation
+import AI.Network as HasNN
+import AI.Calculation as HasNN
 import qualified Data.Packed.Vector as V
-#endif
 
 readDoublesRow :: String -> [Double]
 readDoublesRow row = map read (words row)
@@ -29,51 +21,36 @@ readDoublesRow row = map read (words row)
 readIntsRow :: String -> [Int]
 readIntsRow row = map read (words row)
 
+parseNetFromFile = do
+  input <- readFile "nn.txt"
+  let input'lines@(sizes'row : rest'rows) = lines input
+      sizes = readIntsRow sizes'row -- sizes in first line
 
--- parseNetFromFile :: String -> (Network Double, [Int])
-parseNetFromFile input = asserts $ result -- (length weights, length weights'split, length biases, neuronCount, layerCount, sizes)
-  where input'lines@(sizes'row : rest'rows) = lines input
-        sizes = readIntsRow sizes'row -- sizes in first line
+      neuronCount = sum sizes
+      layerCount = length sizes
 
-        neuronCount = sum sizes
-        layerCount = length sizes
+      rest'double'rows :: [[Double]]
+      rest'double'rows = map readDoublesRow rest'rows
 
-        rest'double'rows :: [[Double]]
-        rest'double'rows = map readDoublesRow rest'rows
+      weights, biases :: [[Double]]
+      (biases,moar) = splitAt layerCount rest'double'rows -- biases in next layerCount lines -- in each appropriate number of biases
+      (weights,garbage) = splitAt neuronCount $ moar -- weights in next neuronCount lines
+      weights'split = splitPlaces sizes weights
 
-        weights, biases :: [[Double]]
-        (biases,moar) = splitAt layerCount rest'double'rows -- biases in next layerCount lines -- in each appropriate number of biases
-        (weights,garbage) = splitAt neuronCount $ moar -- weights in next neuronCount lines
-        weights'split = splitPlaces sizes weights
+      --- weights, biases, neuronCount, layerCount, sizes
+      biases'neg = (map (map negate) biases)  -- must negate biases -- different from matlab
 
-        --- weights, biases, neuronCount, layerCount, sizes
-        biases'neg = (map (map negate) biases)  -- must negate biases -- different from matlab
+      network'01 = Network { activation = getActivation Sigmoid
+                           , derivative = getDerivative Sigmoid
+                           , lambda = 0
+                           , weights = V.fromList (concat weights ++ concat biases)
+                           , architecture = (183:sizes)
+                           }
 
+      network'02 = loadNetwork biases'neg weights'split
 
-#ifdef BENCH01
-#error BENCH01 not finished
-#endif
-
-#ifdef BENCH02
-        result = computeNetworkWith network sigmoid . U.fromList
-        network = loadNetwork biases'neg weights'split
-#endif
-
-#ifdef BENCH03
-        result = network
-        network = Network { activation = getActivation Sigmoid
-                          , derivative = getDerivative Sigmoid
-                          , lambda = 0
-                          , weights = V.fromList (concat weights)
-                          , architecture = sizes
-                          }
-#endif
-
-        asserts r | garbage /= [] = error (printf "parseNetFromFile: garbage not empty: %d elements" (length garbage))
-                  | length weights /= neuronCount = error (printf "parseNetFromFile: too little weights: %d (should be %d)" (length weights) neuronCount)
-                  | length weights'split /= layerCount = error "parseNetFromFile: not enough weights?"
-                  | otherwise = r -- r for result
-
+  print (length weights, length weights'split, length biases, neuronCount, layerCount, sizes)
+  return $! (network'01, network'02)
 
 fib :: Int -> Int
 fib 0 = 0
@@ -81,9 +58,12 @@ fib 1 = 1
 fib n = fib (n-1) + fib (n-2)
 
 main = do
-  network <- parseNetFromFile `fmap` readFile "nn.txt"
-  vec <- withSystemRandom . asGenIO $ \gen -> uniformVector gen (3*61)
+  (network'01, network'02) <- parseNetFromFile
+  vec'01 <- withSystemRandom . asGenIO $ \gen -> uniformVector gen (3*61)
+  vec'02 <- withSystemRandom . asGenIO $ \gen -> uniformVector gen (3*61)
   defaultMain [
-        bench "network" $ nf (networkOutput (network :: Network)) vec,
+        bench "network'01" $ whnf (networkOutput network'01) vec'01,
+        bench "network'02" $ whnf (computeNetworkWith network'02 sigmoid) vec'02,
+        bench "network'03" $ whnf (computeNetworkWith network'02 sigmoid) vec'02,
         bench "fib" $ nf fib 20
        ]
