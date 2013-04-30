f10f2a18058e4a40a46e1e677607a8ea1ee6e050
Krzysztof Skrzętnicki <gtener@gmail.com>
2013-04-30 19:06:17 +0200

    parallel driver for breakthrough-driver, counting how many games each player wins

--- src/breakthrough-driver.hs
+++ src/breakthrough-driver.hs
@@ -5,27 +5,62 @@ import AgentGeneric
 import BreakthroughGame
 
 import Data.Default
+import Data.IORef
+import Data.List (sort, group, groupBy)
+
+import Control.Monad
+import Control.Arrow
+import Control.Concurrent
+import Control.Concurrent.Async
 
 params :: GameParams Breakthrough
 params = (6,6) -- board size
 
-main :: IO ()
 main = do
+  tCount <- getNumCapabilities
+  let gCount = 100000
+      gtCount = gCount `div` tCount
+      gtCountExtra = gCount - (gtCount * tCount)
+
+      -- first thread do extra jobs
+      ttGCount 1 = gtCountExtra + gtCount
+      ttGCount _ = gtCount
+
+      threadJob tNum = do
+         a1 <- mkAgent () :: IO AgentRandom
+         a2 <- mkAgent () :: IO AgentRandom
+         winners <- sequence $ replicate (ttGCount tNum) (oneGame a1 a2)
+         return winners
+
+  asyncThreads <- mapM (async . threadJob) [1..tCount]
+  all'winners <- mapM wait asyncThreads
+  let counts = map (head &&& length) $ group $ sort $ concat all'winners
+  
+  print counts
+
+  return ()
+
+oneGame :: (Agent2 a1, Agent2 a2) => a1 -> a2 -> IO (Maybe Player2)
+oneGame a1 a2 = do
+  ref <- newIORef Nothing
+
   let g0 = freshGame params :: Breakthrough
       cb :: GameDriverCallback Breakthrough
       cb = GameDriverCallback { gameTurn = (\ g p -> do
-                               putStrLn (prettyPrintGame (g :: Breakthrough))
+                               -- putStrLn (prettyPrintGame (g :: Breakthrough))
                                return True)
                , gameFinished = (\ g -> do
-                               putStrLn $ "Game finished! Winner: " ++ show (winner g)
-                               putStrLn (prettyPrintGame (g :: Breakthrough))
+                               let w = winner g
+                               putStrLn $ "Game finished! Winner: " ++ show w
+                               -- putStrLn (prettyPrintGame (g :: Breakthrough))
+                               writeIORef ref w
 
                                 )
                }
 
   
 
-  a1 <- mkAgent () :: IO AgentRandom
-  a2 <- mkAgent () :: IO AgentRandom
 
   driverG2 g0 a1 a2 cb
+
+  readIORef ref
