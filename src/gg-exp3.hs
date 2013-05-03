ccb62c8a8d391beb7ae038a7d113821208b8d081
Krzysztof Skrzętnicki <gtener@gmail.com>
2013-05-03 22:59:47 +0200

    vastly expanded gg-exp3: cached intermidiate values, bugfix local search, timeouts, always disabled buffering

--- src/gg-exp3.hs
+++ src/gg-exp3.hs
@@ -17,6 +17,12 @@ import Control.Monad
 import Control.Concurrent
 import Control.Concurrent.Async
 import Text.Printf
+import System.IO
+import System.Directory
+import System.FilePath
+
+import qualified Control.Concurrent.Timeout as Timeout
+import Data.Timeout
 
 type MyGameA = Abalone
 type MyGameB = Breakthrough
@@ -25,16 +31,28 @@ type MyGame = MyGameB
 someGame :: MyGame
 someGame = freshGameDefaultParams
 
+printFlush p = print p >> hFlush stdout
+
+constraintsCacheFilename = ("tmp-data" </> "cache" </> "constraints-" ++ gameName someGame <.> "txt")
+
 genConstraints = do
   cR <- newIORef []
   let addConstraints cs = atomicModifyIORef cR (\ !old -> ((cs:old),()))
-  sampleRandomGamesCount 100 0.01 (\ g -> do
-                                     print "gen start"
+  sampleRandomGamesCount 150 0.01 (\ g -> do
                                      cs <- generateConstraintsMCTS 1000 (g :: MyGame)
                                      addConstraints cs
-                                     print =<< length `fmap` readIORef cR
+                                     printFlush =<< length `fmap` readIORef cR
                                   )
-  readIORef cR
+  c <- readIORef cR
+  createDirectoryIfMissing True (takeDirectory constraintsCacheFilename)
+  writeFile constraintsCacheFilename (show c)
+  return c
+
+genConstraintsCached = do
+  b <- doesFileExist constraintsCacheFilename
+  if b then read `fmap` readFile constraintsCacheFilename
+       else genConstraints
+
 
 evaluateWinnersCount = 1000 :: Int
 
@@ -42,10 +60,18 @@ calculateWinnersPCT ref g _dep = when (winner (g :: MyGame) == Just P1) (atomicM
 reportWinnersPCT ref = do
   winCount <- readIORef ref
   let winPCT = 100 * ((fromIntegral winCount) / (fromIntegral evaluateWinnersCount)) :: Double
+  putStrLn "======================================================================================"
   putStrLn (printf "P1 won in %d matches, win percentage: %f%%" (winCount :: Int) winPCT :: String)
+  putStrLn "======================================================================================"
+  hFlush stdout
+  -- threadDelay (10000 * (10^6))
 
 main = do
-  constraints <- genConstraints
+  hSetBuffering stdout NoBuffering
+
+  printFlush "Constraint generation"
+  let useConstraintCache = True
+  constraints <- if useConstraintCache then genConstraintsCached else genConstraints 
 
   let fnBr = return "tmp-data/irlfjflptuwgzpqzejrd/dbn.txt"
       fnAb = return "tmp-data/mlubiwjdnaaovrlgsqxu/dbn.txt"
@@ -54,8 +80,9 @@ main = do
       isAbalone = toRepr someGame == toRepr (freshGameDefaultParams :: Abalone)
       isBreakthrough = toRepr someGame == toRepr (freshGameDefaultParams :: Breakthrough)
 
-      useCachedDBN = True
+      useCachedDBN = False
 
+  printFlush "DBN read/train"
   fn <- case (isAbalone, isBreakthrough, useCachedDBN) of
           (True, False, True) -> fnAb
           (False, True, True) -> fnBr
@@ -64,25 +91,46 @@ main = do
   let searchCB ref = (\ new@(bnNew,bsNew,acNew) -> do
                         let newTrim = (bnNew, bsNew)
                         updated <- atomicModifyIORef 
-                                    ref (\ old@(bnOld, bsOld) -> do
+                                    ref (\ old@(bnOld, !bsOld) -> do
                                                        if bsOld < bsNew
                                                         then (newTrim, True)
                                                         else (old,False))
-                        when updated (print newTrim >> acNew)
+                        when updated (printFlush newTrim >> acNew)
                      )
 
+  printFlush "forever train last layer network & evaluate"
   forever $ do
     threads <- getNumCapabilities
-    let thrG = 0.8
-        thrL = 0.95
-        localSearch = 0.08
     bestRef <- newIORef (undefined, neginf)
-    (_, best) <- waitAnyCancel =<< (mapM (\ thr -> async (singleNeuronRandomSearch (searchCB bestRef) thrG thr fn constraints)) [1..threads])
-    (_, bestLocal) <- waitAnyCancel =<< (mapM (\ thr -> async (singleNeuronLocalSearch (searchCB bestRef) bestRef localSearch thrL thr fn constraints)) [1..threads])
+    let thrG = 0.80
+        thrL = 0.90
+        localSearch = 0.18
+
+        withTimeout act = do
+              asyncs <- act 
+              let loop = do
+                    best'0 <- snd `fmap` readIORef bestRef 
+                    delay
+                    best'1 <- snd `fmap` readIORef bestRef 
+                    if best'0 == best'1 
+                     then do
+                       printFlush "UNABLE TO IMPROVE, TIMEOUT"
+                       readIORef bestRef 
+                     else loop
+                  delay = Timeout.threadDelay (1 # Minute)
+              timeoutAsync <- async loop
+              return (timeoutAsync:asyncs)
+
+                                      
+    (_, best) <- waitAnyCancel =<< withTimeout (mapM (\ thr -> async (singleNeuronRandomSearch (searchCB bestRef) thrG thr fn constraints)) [1..threads])
+    (_, bestLocal) <- waitAnyCancel =<< withTimeout (mapM (\ thr -> async (singleNeuronLocalSearch (searchCB bestRef) bestRef localSearch thrL thr fn constraints)) [1..threads])
 
     let llNetwork = uncurry mkTNetwork (fst bestLocal)
+
+    printFlush "BEGIN EVALUATE"
+        
     (dbn, _) <- parseNetFromFile `fmap` readFile fn 
-    myTrainedAgent <- mkAgent (dbn, llNetwork) :: IO AgentSimpleLL
+    myTrainedAgent <- mkAgent (appendNetwork dbn llNetwork) :: IO AgentSimple
     agRnd <- mkAgent () :: IO AgentRandom
 
     winRef <- newIORef 0
