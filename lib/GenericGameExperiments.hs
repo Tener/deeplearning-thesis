1cc12e48e4965d3fc606dc1409653ff98b407d68
Krzysztof Skrzętnicki <gtener@gmail.com>
2013-05-03 07:06:30 +0200

    add optional compression for files (akin gzip)

--- lib/GenericGameExperiments.hs
+++ lib/GenericGameExperiments.hs
@@ -5,14 +5,14 @@ module GenericGameExperiments where
 
 import AgentGeneric
 import GenericGame
---import MinimalNN
 import Matlab
--- import Config
 
 import Data.Default
 import System.FilePath
 import System.Directory
 import qualified Data.ByteString.Char8 as BSC8
+import qualified Data.ByteString.Lazy as BSL
+import qualified Codec.Compression.GZip as GZip
 import Data.IORef
 import Control.Monad
 import System.Random.MWC
@@ -21,15 +21,23 @@ import System.IO
 getRandomFileName :: IO String
 getRandomFileName = (map toEnum) `fmap` replicateM 20 (withSystemRandom $ asGenIO $ uniformR (fromEnum 'a',fromEnum 'z'))
 
+-- | compress file with gzip removing original file.
+compressRemoveFile :: FilePath -> IO FilePath
+compressRemoveFile file'orig = do
+  let file'out = file'orig <.> "gzip"
+  BSL.writeFile file'out =<< GZip.compress `fmap` BSL.readFile file'orig
+  removeFile file'orig
+  return file'out
+
 -- | train DBN on randomly sampled @sampleCount@ games of type @game@. Returns filepath with DBN.
 sampleGamesTrainNetwork :: (Repr (GameRepr g), Game2 g) => g -> Int -> Float -> IO FilePath
 sampleGamesTrainNetwork game sampleCount prob = do
-  sR <- newIORef sampleCount
   outputDir <- ("tmp-data" </>) `fmap` getRandomFileName
   createDirectoryIfMissing True outputDir
   filename'data <- (\f -> outputDir </> f <.> "csv") `fmap` getRandomFileName
 
   withFile filename'data WriteMode $ \ han -> do
+      sR <- newIORef sampleCount
       let cb g = do
             BSC8.hPutStrLn han (serializeGame (ofType g game))
             modifyIORef sR (\d -> d-1)
@@ -40,6 +48,7 @@ sampleGamesTrainNetwork game sampleCount prob = do
 
       sampleRandomGames ((>0) `fmap` readIORef sR) prob cb
       hFlush han
-  
+
+--  filename'data'comp <- compressRemoveFile filename'data
   print =<< prepAndRun def outputDir filename'data
   return (outputDir </> "dbn.txt")
