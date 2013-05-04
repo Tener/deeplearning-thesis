d05eaac19e6e3f685b707320781983566fc3b87e
Krzysztof Skrzętnicki <gtener@gmail.com>
2013-05-04 11:38:01 +0200

    inline pragmas in MinimalNN

--- lib/MinimalNN.hs
+++ lib/MinimalNN.hs
@@ -20,6 +20,7 @@ instance Ord (Matrix Double) where
 
 computeTNetworkSigmoid :: TNetwork -> Vector Double -> Vector Double
 computeTNetworkSigmoid (TNetwork ws bs) inp0 = foldl (\ inp (w,b) -> cmap sigmoid ((inp `vXm` w) `add` b) ) inp0 (zip ws bs)
+{-# INLINE computeTNetworkSigmoid #-}
 
 computeTNetworkSigmoidSteps :: Int -> TNetwork -> Vector Double -> Vector Double
 computeTNetworkSigmoidSteps steps (TNetwork ws bs) inp0 = foldl (\ inp (w,b) -> cmap sigmoid ((inp `vXm` w) `add` b) ) inp0 (take steps $ zip ws bs)
@@ -28,12 +29,14 @@ computeTNetworkSigmoidSteps steps (TNetwork ws bs) inp0 = foldl (\ inp (w,b) -> 
 mkTNetwork :: [[[Double]]] -> [[Double]] -> TNetwork
 mkTNetwork w b | length w == length b = TNetwork (map (Matrix.trans . Matrix.fromLists) w) (map Vector.fromList b)
                | otherwise = error "Inequal number of layers for biases and weights"
+{-# INLINE mkTNetwork #-}
 
 -- | append one network after another. input/output sizes must work out (which isn't checked until later)
 appendNetwork :: TNetwork -- ^ first network, will receive input first
               -> TNetwork -- ^ second network, will be fed output of first network
               -> TNetwork -- ^ resulting network
 appendNetwork tnet1 tnet2 = TNetwork (weights tnet1 ++ weights tnet2) (biases tnet1 ++ biases tnet2)
+{-# INLINE appendNetwork #-}
 
 -- | return last layer in nn, assuming it is one neuron big
 lastLayerTN :: TNetwork -> [(Double,[Double])]
