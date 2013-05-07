1483a9a73b495ca41da832fc28b3b28498c4bb02
Krzysztof Skrzętnicki <gtener@gmail.com>
2013-05-07 15:30:16 +0200

    expand gg-exp3 experiment (TODO: extract common functions)

--- src/gg-exp3.hs
+++ src/gg-exp3.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE ImplicitParams, Rank2Types, BangPatterns #-}
 
 module Main where
 
@@ -10,7 +10,9 @@ import GenericGame
 import AgentGeneric
 import ConstraintsGeneric
 import MinimalNN
+import Matlab
 import NeuralNets (parseNetFromFile)
+import ThreadLocal
 
 import Data.IORef
 import Control.Monad
@@ -20,65 +22,95 @@ import Text.Printf
 import System.IO
 import System.Directory
 import System.FilePath
+import Data.Default
 
 import qualified Control.Concurrent.Timeout as Timeout
 import Data.Timeout
+import Data.Chronograph
 
 type MyGameA = Abalone
 type MyGameB = Breakthrough
 type MyGame = MyGameB
 
+
+
 someGame :: MyGame
 someGame = freshGameDefaultParams
 
-printFlush p = print p >> hFlush stdout
-
-
 constraintCount, constraintDepth :: Int
-constraintCount = 100
-constraintDepth = 100
+constraintCount = 500
+constraintDepth = 1000
 
+constraintsCacheFilename :: FilePath
 constraintsCacheFilename = let spec :: String
                                spec = printf "constraints-d%d-c%d-%s.txt" constraintDepth constraintCount (gameName someGame)
                            in "tmp-data" </> "cache" </> spec
 
-genConstraints = do
+
+genConstraints :: ThrLocIO [(MyGame, MyGame)]
+genConstraints = concat `fmap` parWorkThreads constraintCount genConstraintsCnt
+
+parWorkThreads :: Int -> (Int -> ThrLocIO a) -> ThrLocIO [a]
+parWorkThreads c fun = do
+  threads <- getNumCapabilities
+  let oneThr = c `div` threads
+      re = c - (oneThr * threads)
+      cnt 1 = oneThr+re
+      cnt _ = oneThr
+  let mvar'stdout = tl_stdout ?thrLoc
+  ccs <- mapConcurrently (\ thr -> do
+                            runThrLocIO (ThreadLocal mvar'stdout (show thr)) (fun (cnt thr)))
+                         [1..threads] 
+  return ccs
+
+genConstraintsCnt :: Int -> ThrLocIO [(MyGame, MyGame)]
+genConstraintsCnt conCount = do
   cR <- newIORef []
   let addConstraints cs = atomicModifyIORef cR (\ !old -> ((cs:old),()))
   
   ag <- mkAgent constraintDepth :: IO AgentMCTS
-  sampleRandomGamesCount constraintCount 0.01 (\ g -> do
+  sampleRandomGamesCount conCount 0.01 (\ g -> do
                                      cs <- generateConstraintsMCTS' ag (g :: MyGame)
                                      addConstraints cs
-                                     printFlush =<< length `fmap` readIORef cR
+                                     printTL =<< length `fmap` readIORef cR
                                   )
   c <- readIORef cR
   createDirectoryIfMissing True (takeDirectory constraintsCacheFilename)
   writeFile constraintsCacheFilename (show c)
   return c
 
+
+genConstraintsCached :: ThrLocIO [(MyGame,MyGame)]
 genConstraintsCached = do
   b <- doesFileExist constraintsCacheFilename
   if b then read `fmap` readFile constraintsCacheFilename
        else genConstraints
 
 
-evaluateWinnersCount = 1000 :: Int
+evaluateWinnersCount :: Int
+evaluateWinnersCount = 100
 
-calculateWinnersPCT pl ref g _dep = when (winner (g :: MyGame) == Just pl) (atomicModifyIORef ref (\ !v -> ((v+1),())))
-reportWinnersPCT pl ref = do
-  winCount <- readIORef ref
+calculateWinnersPCT pl ref g d = do
+  let w = if (winner (g :: MyGame) == Just pl) then 1 else 0
+
+  wins <- atomicModifyIORef ref (\ (!cnt, !dep) -> ((cnt+w, dep+d), (cnt+w)))
+  if (w == 1) then (printTL ("WINS",wins)) else printTL ("LOSE",d)
+
+
+reportWinnersPCT ag1 ag2 pl ref = do
+  (winCount,depths) <- readIORef ref
   let winPCT = 100 * ((fromIntegral winCount) / (fromIntegral evaluateWinnersCount)) :: Double
-  putStrLn "======================================================================================"
-  putStrLn (printf "%s won in %d matches, win percentage: %f%%" (show pl) (winCount :: Int) winPCT :: String)
-  putStrLn "======================================================================================"
+      depthAVG = (fromIntegral depths) / (fromIntegral evaluateWinnersCount) :: Double
+      n1 = if pl == P1 then agentName ag1 else agentName ag2
+      n2 = if pl == P2 then agentName ag1 else agentName ag2
+  putStrLn (printf "%s[%s] won vs [%s] in %d matches, win percentage: %f%%, avg depth=%f" (show pl) n1 n2 (winCount :: Int) winPCT depthAVG :: String)
   hFlush stdout
-  -- threadDelay (10000 * (10^6))
-
-main = do
+  
+main :: IO ()
+main = runThrLocMainIO $ do
   hSetBuffering stdout NoBuffering
 
-  printFlush "Constraint generation"
+  printTL "Constraint generation"
   let useConstraintCache = True
   constraints <- if useConstraintCache then genConstraintsCached else genConstraints 
 
@@ -91,30 +123,32 @@ main = do
 
       useCachedDBN = False
 
-  printFlush "DBN read/train"
+  printTL "DBN read/train"
   fn <- case (isAbalone, isBreakthrough, useCachedDBN) of
           (True, False, True) -> fnAb
           (False, True, True) -> fnBr
           (_, _, _) -> fnTN
 
-  print ("DBN FN=",fn)
+  printTL ("DBN FN=",fn)
 
-  let searchCB ref = (\ new@(bnNew,bsNew,acNew) -> do
+  let searchCB ref = (\ (bnNew,bsNew,acNew) -> do
                         let newTrim = (bnNew, bsNew)
                         updated <- atomicModifyIORef 
-                                    ref (\ old@(bnOld, !bsOld) -> do
+                                    ref (\ old@(_bnOld, !bsOld) -> do
                                                        if bsOld < bsNew
                                                         then (newTrim, True)
                                                         else (old,False))
-                        when updated (printFlush newTrim >> acNew)
+                        when updated (printTL newTrim >> acNew)
                      )
 
-  printFlush "forever train last layer network & evaluate"
-  forever $ do
+  printTL "forever train last layer network & evaluate"
+  let timestamp = getModificationTime "src/gg-exp3.hs"
+  ts0 <- timestamp
+  while ((==ts0) `fmap` timestamp) $ do
     threads <- getNumCapabilities
     bestRef <- newIORef (undefined, neginf)
-    let thrG = 0.80
-        thrL = 0.90
+    let thrG = 1
+        thrL = 1
         localSearch = 0.18
 
         withTimeout act = do
@@ -125,10 +159,10 @@ main = do
                     best'1 <- snd `fmap` readIORef bestRef
                     if best'0 == best'1 
                      then do
-                       printFlush "UNABLE TO IMPROVE, TIMEOUT"
+                       printTL "UNABLE TO IMPROVE, TIMEOUT"
                        readIORef bestRef 
                      else loop
-                  delay = Timeout.threadDelay ((constraintCount `div` 5) # Seconds)
+                  delay = Timeout.threadDelay (30 # Second)
               timeoutAsync <- async loop
               return (timeoutAsync:asyncs)
 
@@ -140,28 +174,43 @@ main = do
         packConstraint c = fmap packGame c
         packGame game = computeTNetworkSigmoid dbn $ reprToNN $ toRepr game
 
-    (_, best) <- waitAnyCancel =<< withTimeout (mapM (\ thr -> async (singleNeuronRandomReprSearch (searchCB bestRef) thrG thr constraintsPacked)) [1..threads])
+    (_, _best) <- waitAnyCancel =<< withTimeout (mapM (\ thr -> async (singleNeuronRandomReprSearch (searchCB bestRef) thrG thr constraintsPacked)) [1..threads])
     (_, bestLocal) <- waitAnyCancel =<< withTimeout (mapM (\ thr -> async (singleNeuronLocalReprSearch (searchCB bestRef) bestRef localSearch thrL thr constraintsPacked)) [1..threads])
 
+    putStrLn $ printf "FINAL SCORE %s" (show bestLocal)
+
+    printTL "BEGIN EVALUATE"
+
     let llNetwork = uncurry mkTNetwork (fst bestLocal)
-
-    printFlush "BEGIN EVALUATE"
+        evalNetwork = appendNetwork dbn llNetwork
+        timed s a = do
+          (Chronograph r t) <- chronoIO a
+          printTL (s,t)
+          return r
         
-    myTrainedAgent <- mkAgent (appendNetwork dbn llNetwork) :: IO AgentSimple
-    agRnd <- mkAgent () :: IO AgentRandom
+        mkTimed label arg = mkAgent ((IOAct (timed label)), arg)
+        
+    agSmpl <- mkTimed "simple" evalNetwork :: IO (AgentTrace AgentSimple)
+    agTree <- mkTimed "tree" (evalNetwork, 5) :: IO (AgentTrace AgentGameTree)
+    
+    agRnd <- mkTimed "random" () :: IO (AgentTrace AgentRandom)
+    agMTC <- mkTimed "mcts" 30 :: IO (AgentTrace AgentMCTS)
 
-    let reportWin pl = do
-              winRef <- newIORef 0
-              sampleGameDepthCount myTrainedAgent agRnd evaluateWinnersCount (calculateWinnersPCT pl winRef)
-              reportWinnersPCT pl winRef
-
-    reportWin P1
-    reportWin P2
+    let reportWin ag ag2 pl = do
+              winRef <- newIORef (0,0)
+              -- sampleGameDepthCount ag ag2 evaluateWinnersCount (calculateWinnersPCT pl winRef)
+              parWorkThreads evaluateWinnersCount (\ cnt -> sampleGameDepthCount ag ag2 cnt (calculateWinnersPCT pl winRef))
+              reportWinnersPCT ag ag2 pl winRef
+    putStrLn "======================================================================================"
+    -- reportWin agSmpl agRnd P1
+    reportWin agSmpl agMTC P1
+    -- reportWin agTree agRnd P1
+    reportWin agTree agMTC P1
+    putStrLn "======================================================================================"
 
               
              
     return ()
 
-  return ()
-
+  printTL "Source file changed, exiting"
   
