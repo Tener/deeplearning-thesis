c0893ac8c5f2f7d0aa0de99e1c34f099f23e9e29
Krzysztof Skrzętnicki <gtener@gmail.com>
2013-05-14 22:03:18 +0200

    fix yesod frontend to match recent changes in master project

--- yesod/abaloney/Handler/Breakthrough.hs
+++ yesod/abaloney/Handler/Breakthrough.hs
@@ -9,14 +9,15 @@ import Text.Blaze.Html
 import BreakthroughGame
 import AgentGeneric
 import GenericGame
+import ThreadLocal
 
 getBreakthroughR :: Int -> Text -> Handler RepHtml
 getBreakthroughR mctsRating reprTxt = do
-  agMCTS <- liftIO $ (mkAgent mctsRating :: IO AgentMCTS)
+  agMCTS <- liftIO $ runThrLocMainIO (mkAgent mctsRating :: IO AgentMCTS)
 
   let bRepr = map read $ S.splitOn "," $ T.unpack $ reprTxt
       game :: Breakthrough
-      game = fromRepr (6,6) bRepr
+      game = fromRepr (8,8) bRepr
       possibleMoves'P1 = moves game P1
       possibleMoves'P2 = moves game P2
 
@@ -24,8 +25,8 @@ getBreakthroughR mctsRating reprTxt = do
 
       prettyPrintGame' g = preEscapedToHtml $ intercalate "<br>" (lines (prettyPrintGame g))
 
-  agMoveP1 <- liftIO $ applyAgent agMCTS game P1
-  agMoveP2 <- liftIO $ applyAgent agMCTS game P2
+  agMoveP1 <- liftIO $ runThrLocMainIO $ applyAgent agMCTS game P1
+  agMoveP2 <- liftIO $ runThrLocMainIO $ applyAgent agMCTS game P2
   
   defaultLayout $ do
     setTitle "So abaloney... breakthrough edition."
