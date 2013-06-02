016470b6e41ddb362b8990965a04f0870001490d
Krzysztof Skrzętnicki <gtener@gmail.com>
2013-06-02 10:09:31 +0200

    heroku build fixes

--- gui/hooks/build.sh
+++ gui/hooks/build.sh
@@ -1,1 +1,2 @@
 echo "-----> Build hook"
+cabal-dev -j --sandbox=$CABAL_DEV_SANDBOX install --disable-library-profiling --disable-executable-profiling --disable-shared --extra-include-dirs=$BUILD_DIR/debs-extra/usr/include --extra-lib-dirs=$BUILD_DIR/debs-extra/usr/lib
--- gui/hooks/start.sh
+++ gui/hooks/start.sh
@@ -1,2 +1,4 @@
 echo "-----> Start hook"
 BUILDPACK_CONF_PURGE_CACHE=yes
+CABAL_DEV_SANDBOX=$BUILD_DIR/cabal-dev
+
