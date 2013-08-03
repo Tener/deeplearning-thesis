0d6a236aff21c261312f6389243e3b397d5da035
Krzysztof Skrzętnicki <gtener@gmail.com>
2013-08-03 07:48:37 +0200

    disable parallel build on heroku

--- gui/hooks/build.sh
+++ gui/hooks/build.sh
@@ -1,2 +1,2 @@
 echo "-----> Build hook"
-cabal-dev -j --sandbox=$CABAL_DEV_SANDBOX install --disable-library-profiling --disable-executable-profiling --disable-shared --extra-include-dirs=$BUILD_DIR/debs-extra/usr/include --extra-lib-dirs=$BUILD_DIR/debs-extra/usr/lib
+cabal-dev --sandbox=$CABAL_DEV_SANDBOX install --disable-library-profiling --disable-executable-profiling --disable-shared --extra-include-dirs=$BUILD_DIR/debs-extra/usr/include --extra-lib-dirs=$BUILD_DIR/debs-extra/usr/lib
